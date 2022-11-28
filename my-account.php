f90ae07a80f2cc8742724108978260fb38e8b9b5
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2022-11-28 01:28:29 +0000

    My Account (Profile Upload) Redesign

--- my-account.php
+++ my-account.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+        <link rel="icon" href="images/FAVICON.png" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>MY ACCOUNT | Vance Social</title>
+</head>
+<body class="landing-body">
+
+	<?php
 include('./classes/DB.php');
 include('./classes/Login.php');
 if (Login::isLoggedIn()) {
@@ -10,9 +19,16 @@ if (isset($_POST['uploadprofileimg'])) {
         Image::uploadImage('profileimg', "UPDATE users SET profileimg = :profileimg WHERE id=:userid", array(':userid'=>$userid));
 }
 ?>
-<h1>My Account</h1>
-<form action="my-account.php" method="post" enctype="multipart/form-data">
-        Upload a profile image:
-        <input type="file" name="profileimg">
-        <input type="submit" name="uploadprofileimg" value="Upload Image">
-</form>
+
+	<div class="login-container">
+		<h1 style="font-size: 30px; padding-left: 20px; padding-right: 20px; color: #E8E8E8;">Upload a profile image</h1>
+		<form action="changetophp" method="post">
+			<div class="landing-input">
+				<input type="file" accept="image/png, image/jpeg" name="profileimg">
+			</div>
+
+			<input id="log-submit" type="submit" name="uploadprofileimg" value="Upload Image">
+		</form>
+	</div>
+</body>
+</html>
